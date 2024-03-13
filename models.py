2572fbe705869e7f82177cff63860545bfcabbf1
Shivdutta <shivdutta@gmail.com>
2024-03-14 02:58:50 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -589,6 +589,88 @@ class Model3(nn.Module):
         x = x.view(-1, 10)
         return F.log_softmax(x, dim=-1)
 
+class Model2(nn.Module):
+    def __init__(self,dropout_value):
+        super(Model2, self).__init__()
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 26
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 24
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 22
+
+        # TRANSITION BLOCK 1
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 11
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 11
+
+        # CONVOLUTION BLOCK 2
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 9
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 7
+
+        # OUTPUT BLOCK
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+            nn.ReLU()
+        ) # output_size = 7
+        self.convblock8 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(7, 7), padding=0, bias=False),
+            # nn.ReLU() NEVER!
+        ) # output_size = 1 7x7x10 | 7x7x10x10 | 1x1x10
+
+    def forward(self, x):
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+        x = self.convblock3(x)
+        x = self.pool1(x)
+        x = self.convblock4(x)
+        x = self.convblock5(x)
+        x = self.convblock6(x)
+        x = self.convblock7(x)
+        x = self.convblock8(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
+        
+class Model1(nn.Module):
+    def __init__(self,dropout_value):
+        super(Model1, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1) # 28>28 | 3
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1) # 28 > 28 |  5
+        self.pool1 = nn.MaxPool2d(2, 2) # 28 > 14 | 10
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1) # 14> 14 | 12
+        self.conv4 = nn.Conv2d(128, 256, 3, padding=1) #14 > 14 | 14
+        self.pool2 = nn.MaxPool2d(2, 2) # 14 > 7 | 28
+        self.conv5 = nn.Conv2d(256, 512, 3) # 7 > 5 | 30
+        self.conv6 = nn.Conv2d(512, 1024, 3) # 5 > 3 | 32 | 3*3*1024 | 3x3x1024x10 |
+        self.conv7 = nn.Conv2d(1024, 10, 3) # 3 > 1 | 34 | > 1x1x10
+
+    def forward(self, x):
+        x = self.pool1(F.relu(self.conv2(F.relu(self.conv1(x)))))
+        x = self.pool2(F.relu(self.conv4(F.relu(self.conv3(x)))))
+        x = F.relu(self.conv6(F.relu(self.conv5(x))))
+        # x = F.relu(self.conv7(x))
+        x = self.conv7(x)
+        x = x.view(-1, 10) #1x1x10> 10
+        return F.log_softmax(x, dim=-1)
+
+
 def get_summary(model, input_size) :   
     """
     Generate Model Summary Using torchsummary.
@@ -607,60 +689,3 @@ def get_summary(model, input_size) :
     return summary(network, input_size=input_size)
 
 
-# class Model10(nn.Module):
-#     def __init__(self):
-#         super(Model10, self).__init__()
-#         self.conv1 = nn.Conv2d(1, 10, 3, padding=0, bias=False)       # 28 > 26 | 3
-#         self.batch_norm1 = nn.BatchNorm2d(10)
-#         self.conv2 = nn.Conv2d(10, 10, 3, padding=0, bias=False)      # 26 > 24 | 5
-#         self.batch_norm2 = nn.BatchNorm2d(10)
-#         self.conv3 = nn.Conv2d(10, 10, 3, padding=0, bias=False)      # 24 > 22 | 7
-#         self.batch_norm3 = nn.BatchNorm2d(10)
-#         self.conv4 = nn.Conv2d(10, 10, 3, padding=0, bias=False)      # 22 > 20 | 9
-#         self.batch_norm4 = nn.BatchNorm2d(10)
-
-#         self.point1 = nn.Conv2d(10, 10, 1, padding=0, bias=False)     # 20 > 20 | 9
-#         self.batch_normp1 = nn.BatchNorm2d(10)
-#         self.pool1 = nn.MaxPool2d(2, 2)                               # 20 > 10 | 10
-
-        
-#         self.conv5 = nn.Conv2d(10, 14, 3, padding=0, bias=False)      # 10 > 8 | 14
-#         self.batch_norm5 = nn.BatchNorm2d(14)
-#         self.conv6 = nn.Conv2d(14, 14, 3, padding=0, bias=False)      #  8 > 6 | 18
-#         self.batch_norm6 = nn.BatchNorm2d(14)
-#         self.conv7 = nn.Conv2d(14, 13, 3, padding=0, bias=False)      #  6 > 4 | 22
-#         self.batch_norm7 = nn.BatchNorm2d(13)
-
-
-#         self.gap = nn.AdaptiveAvgPool2d(1)                            # 4 > 1 | 28
-
-#         self.point2 = nn.Conv2d(13, 10, 1, padding=0, bias=False)     # 1 > 1 | 28
-#         self.batch_normp2 = nn.BatchNorm2d(10)
-
-#         # self.flat = nn.Flatten()
-#         # self.fc = nn.Linear(13, 10)
-
-#         # self.dropout = nn.Dropout(0.25)
-
-
-#     def forward(self, x):
-#         x = F.relu(self.batch_norm1(self.conv1(x)))
-#         x = F.relu(self.batch_norm2(self.conv2(x)))
-#         x = F.relu(self.batch_norm3(self.conv3(x)))
-#         #x = self.dropout(x)
-#         x = self.batch_norm4(self.conv4(x))
-
-#         x = self.pool1(self.batch_normp1(self.point1(x)))
-
-#         x = F.relu(self.batch_norm5(self.conv5(x)))
-#         x = F.relu(self.batch_norm6(self.conv6(x)))
-#         #x = self.dropout(x)
-#         x = self.batch_norm7(self.conv7(x))
-
-#         x = self.gap(x)
-
-#         x = self.batch_normp2(self.point2(x))
-#         # x = self.flat(x)
-#         # x = self.fc(x)
-#         x = x.view(-1, 10)                                             # 1x1x10 > 10
-#         return F.log_softmax(x, dim=-1)
