f93346128b1db433768578525f1ff4fa0b893c29
Shivdutta <shivdutta@gmail.com>
2024-03-15 01:57:29 +0530

    Add files via upload

--- utils.py
+++ utils.py
@@ -7,6 +7,18 @@ from tqdm import tqdm
 import torch.nn.functional as F
 
 def plot_accuracy_losses(train_losses,train_acc,test_losses,test_acc):
+  """
+    Plots the training and test losses along with training and test accuracies.
+
+    Args:
+        train_losses (list): List of training losses.
+        train_acc (list): List of training accuracies.
+        test_losses (list): List of test losses.
+        test_acc (list): List of test accuracies.
+
+    Returns:
+        None
+  """
   t = [t_items.item() for t_items in train_losses]
   fig, axs = plt.subplots(2,2,figsize=(15,10))
   axs[0, 0].plot(t)
@@ -19,6 +31,20 @@ def plot_accuracy_losses(train_losses,train_acc,test_losses,test_acc):
   axs[1, 1].set_title("Test Accuracy")
 
 def train(model, device, train_loader, optimizer, epoch,scheduler):
+  """
+    Trains the specified model using the given data loader and optimizer for one epoch.
+
+    Args:
+        model (torch.nn.Module): The neural network model to train.
+        device (torch.device): The device to perform training on (e.g., "cuda" or "cpu").
+        train_loader (torch.utils.data.DataLoader): DataLoader for training data.
+        optimizer (torch.optim.Optimizer): Optimizer to use for training.
+        epoch (int): Current epoch number.
+        scheduler (torch.optim.lr_scheduler._LRScheduler, optional): Learning rate scheduler (default: None).
+
+    Returns:
+        tuple: A tuple containing the training accuracy and losses as lists.
+  """
   train_losses = []
   train_acc = []
   model.train()
@@ -39,7 +65,7 @@ def train(model, device, train_loader, optimizer, epoch,scheduler):
 
     # Calculate loss
 
-    #below 2 lines are for de
+    #below 2 lines are for debug
     #print('y_pred.shape',y_pred.shape)
     #print('target.shape',target.shape)
     loss = F.nll_loss(y_pred, target)
@@ -64,6 +90,17 @@ def train(model, device, train_loader, optimizer, epoch,scheduler):
   return train_acc,train_losses  
 
 def test(model, device, test_loader):    
+    """
+    Evaluates the specified model on the test dataset.
+
+    Args:
+        model (torch.nn.Module): The neural network model to evaluate.
+        device (torch.device): The device to perform evaluation on (e.g., "cuda" or "cpu").
+        test_loader (torch.utils.data.DataLoader): DataLoader for test data.
+
+    Returns:
+        tuple: A tuple containing the test accuracy and loss as lists.
+    """
     test_losses = []
     test_acc = []   
     model.eval()
